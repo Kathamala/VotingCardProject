1c66b86d204a1d3237bd80153e904f5e459bcb51
Thiago Vinicius <thiagovao2000@hotmail.com>
2021-07-02 16:04:02 -0300

    Implementação do voting-card usando vue

---
+++ voting-card/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <Card
+      :title="title"
+      :state="state"
+      :votes="votes"
+    ></Card>
+  </div>
+</template>
+
+<script>
+  import Card from './components/voting-card.vue'
+
+  export default {
+    name: 'App',
+    data: () => ({
+      title: "Segue o relator?",
+      state: "open",
+      votes: [
+        {
+          "option": "Sim",
+          "count": 8
+        },
+        {
+          "option": "Não",
+          "count": 3
+        }
+      ]
+    }),
+    components: {
+      Card
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ voting-card/src/components/voting-booth.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="booth">
+        <button 
+            class="choices"
+            v-for="(option, index) in options" 
+            :key="option"
+            @click="$emit('select', index)"
+        >
+            {{ option }}
+        </button>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        props: ['options']
+    }
+
+</script>
+
+<style>
+    .choices{
+        margin: 10px;
+    }  
+</style>
